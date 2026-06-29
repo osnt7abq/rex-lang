7a36621f25b09478d7adec7d8579e4cc6337cb10
AIRA <osantbaghel8@gmail.com>
2026-06-29 15:23:47 +0530

    Create interpreter/main.py

---
+++ interpreter/main.py
@@ -0,0 +1,14 @@
+"""
+🦖 Rex Programming Language
+Version: 0.1 "Hatchling"
+
+Main Interpreter
+"""
+
+def main():
+    print("🦖 Welcome to Rex!")
+    print("Version: 0.1 'Hatchling'")
+    print("Status: Development has begun.")
+
+if __name__ == "__main__":
+    main()
