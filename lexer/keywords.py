518b3091a09c092dd47ccd75df8ff610bb0ebd82
AIRA <osantbaghel8@gmail.com>
2026-06-29 15:29:34 +0530

    Create lexer/keywords.py

---
+++ lexer/keywords.py
@@ -0,0 +1,24 @@
+"""
+🦖 Rex Programming Language
+Keywords
+Version: 0.1 "Hatchling"
+"""
+
+KEYWORDS = {
+    "let",
+    "const",
+    "func",
+    "if",
+    "else",
+    "for",
+    "while",
+    "return",
+    "import",
+    "true",
+    "false",
+    "null",
+    "and",
+    "or",
+    "not",
+    "print"
+}
