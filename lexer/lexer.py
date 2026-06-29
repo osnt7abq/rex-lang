f40a33de720c3a45ec8c9eaa9b32ddb5ff678c20
AIRA <osantbaghel8@gmail.com>
2026-06-29 15:26:30 +0530

    Create lexer/lexer.py

---
+++ lexer/lexer.py
@@ -0,0 +1,12 @@
+"""
+🦖 Rex Programming Language
+Lexer
+Version: 0.1 "Hatchling"
+"""
+
+class Lexer:
+    def __init__(self, source):
+        self.source = source
+
+    def tokenize(self):
+        print("Tokenizing Rex source code...")
