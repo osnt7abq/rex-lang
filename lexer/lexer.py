049f0534d2c83d18e562de809a15ca2fe36507dd
AIRA <osantbaghel8@gmail.com>
2026-06-30 08:15:26 +0530

    Update lexer.py

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -4,9 +4,24 @@ Lexer
 Version: 0.1 "Hatchling"
 """
 
+from keywords import KEYWORDS
+from token import Token
+from token_types import TokenType
+
+
 class Lexer:
     def __init__(self, source):
-        self.source = source
+        self.words = source.split()
 
     def tokenize(self):
-        print("Tokenizing Rex source code...")
+        tokens = []
+
+        for word in self.words:
+            if word in KEYWORDS:
+                tokens.append(Token(TokenType.KEYWORD, word))
+            elif word.isdigit():
+                tokens.append(Token(TokenType.INTEGER, word))
+            else:
+                tokens.append(Token(TokenType.IDENTIFIER, word))
+
+        return tokens
