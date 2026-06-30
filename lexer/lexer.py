6ed37e6e67c41573b64544a81e9b8c1618d5968d
AIRA <osantbaghel8@gmail.com>
2026-06-30 14:05:03 +0530

    Update lexer.py

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -19,34 +19,76 @@ class Lexer:
         for word in self.words:
             if word in KEYWORDS:
                 tokens.append(Token(TokenType.KEYWORD, word))
+
             elif word == "=":
                 tokens.append(Token(TokenType.ASSIGN, word))
+
             elif word == "+":
                 tokens.append(Token(TokenType.PLUS, word))
+
             elif word == "-":
                 tokens.append(Token(TokenType.MINUS, word))
+
             elif word == "*":
                 tokens.append(Token(TokenType.MULTIPLY, word))
+
             elif word == "/":
                 tokens.append(Token(TokenType.DIVIDE, word))
+
             elif word == "%":
                 tokens.append(Token(TokenType.MODULO, word))
-elif word == "==":
-    tokens.append(Token(TokenType.EQUAL, word))
-elif word == "!=":
-    tokens.append(Token(TokenType.NOT_EQUAL, word))
-elif word == ">":
-    tokens.append(Token(TokenType.GREATER, word))
-elif word == "<":
-    tokens.append(Token(TokenType.LESS, word))
-elif word == ">=":
-    tokens.append(Token(TokenType.GREATER_EQUAL, word))
-elif word == "<=":
-    tokens.append(Token(TokenType.LESS_EQUAL, word))
-elif word.startswith('"') and word.endswith('"'):
+
+            elif word == "==":
+                tokens.append(Token(TokenType.EQUAL, word))
+
+            elif word == "!=":
+                tokens.append(Token(TokenType.NOT_EQUAL, word))
+
+            elif word == ">":
+                tokens.append(Token(TokenType.GREATER, word))
+
+            elif word == "<":
+                tokens.append(Token(TokenType.LESS, word))
+
+            elif word == ">=":
+                tokens.append(Token(TokenType.GREATER_EQUAL, word))
+
+            elif word == "<=":
+                tokens.append(Token(TokenType.LESS_EQUAL, word))
+
+            elif word == "(":
+                tokens.append(Token(TokenType.LPAREN, word))
+
+            elif word == ")":
+                tokens.append(Token(TokenType.RPAREN, word))
+
+            elif word == "{":
+                tokens.append(Token(TokenType.LBRACE, word))
+
+            elif word == "}":
+                tokens.append(Token(TokenType.RBRACE, word))
+
+            elif word == "[":
+                tokens.append(Token(TokenType.LBRACKET, word))
+
+            elif word == "]":
+                tokens.append(Token(TokenType.RBRACKET, word))
+
+            elif word == ",":
+                tokens.append(Token(TokenType.COMMA, word))
+
+            elif word == ".":
+                tokens.append(Token(TokenType.DOT, word))
+
+            elif word == ":":
+                tokens.append(Token(TokenType.COLON, word))
+
+            elif word.startswith('"') and word.endswith('"'):
                 tokens.append(Token(TokenType.STRING, word[1:-1]))
+
             elif word.isdigit():
                 tokens.append(Token(TokenType.INTEGER, word))
+
             else:
                 tokens.append(Token(TokenType.IDENTIFIER, word))
 
