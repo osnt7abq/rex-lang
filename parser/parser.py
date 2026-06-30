6dd57f7fd2d820410ef97a963c42a0399d529745
AIRA <osantbaghel8@gmail.com>
2026-06-30 14:38:45 +0530

    Update parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -17,3 +17,58 @@ class Parser:
         if self.position < len(self.tokens):
             return self.tokens[self.position]
         return None
+
+    def advance(self):
+        self.position += 1
+        return self.current()
+
+    def match(self, token_type):
+        token = self.current()
+
+        if token and token.type == token_type:
+            self.advance()
+            return token
+
+        return None
+
+    def parse(self):
+        program = Program()
+
+        while self.current() is not None:
+            statement = self.parse_statement()
+
+            if statement:
+                program.statements.append(statement)
+            else:
+                self.advance()
+
+        return program
+
+    def parse_statement(self):
+        token = self.current()
+
+        if token and token.type == TokenType.KEYWORD:
+            if token.value == "let":
+                return self.parse_variable_declaration()
+
+        return None
+
+    def parse_variable_declaration(self):
+        self.advance()  # Skip 'let'
+
+        name = self.match(TokenType.IDENTIFIER)
+
+        self.match(TokenType.ASSIGN)
+
+        value = self.current()
+
+        if value.type == TokenType.INTEGER:
+            value = IntegerLiteral(value.value)
+        elif value.type == TokenType.STRING:
+            value = StringLiteral(value.value)
+        else:
+            value = Identifier(value.value)
+
+        self.advance()
+
+        return VariableDeclaration(name.value, value)
