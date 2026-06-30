2fca63394c9dc853f7323cc4dc0aff7d3db3238c
AIRA <osantbaghel8@gmail.com>
2026-06-30 14:08:17 +0530

    Create ast.py

---
+++ parser/ast.py
@@ -0,0 +1,31 @@
+"""
+🦖 Rex Programming Language
+AST Nodes
+Version: 0.1 "Hatchling"
+"""
+
+
+class Program:
+    def __init__(self):
+        self.statements = []
+
+
+class VariableDeclaration:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+
+class IntegerLiteral:
+    def __init__(self, value):
+        self.value = value
+
+
+class StringLiteral:
+    def __init__(self, value):
+        self.value = value
+
+
+class Identifier:
+    def __init__(self, name):
+        self.name = name
