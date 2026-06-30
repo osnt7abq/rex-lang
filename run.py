d2fa5cbd41935d7ca8db1fd6eeb2e6acc60766de
AIRA <osantbaghel8@gmail.com>
2026-06-30 19:39:12 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,12 +6,14 @@ Version: 0.1 "Hatchling"
 
 from lexer.lexer import Lexer
 from parser.parser import Parser
+from interpreter.interpreter import Interpreter
 
 
 def main():
     with open("examples/hello.rex", "r") as file:
         code = file.read()
 
+    # Lexer
     lexer = Lexer(code)
     tokens = lexer.tokenize()
 
@@ -19,18 +21,20 @@ def main():
     for token in tokens:
         print(token)
 
+    # Parser
     parser = Parser(tokens)
     program = parser.parse()
-
-interpreter = Interpreter()
-interpreter.execute(program)
-
-print("\n=== VARIABLES ===")
-print(interpreter.variables)
 
     print("\n=== AST ===")
     print(program)
 
+    # Interpreter
+    interpreter = Interpreter()
+    interpreter.execute(program)
+
+    print("\n=== VARIABLES ===")
+    print(interpreter.variables)
+
 
 if __name__ == "__main__":
-    main()
+    main() 
